213dee06443908fc2aaf1a5873dbb49e5be80b1f
Christian Weiss <worldmover777@gmail.com>
2018-02-16 18:25:12 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,4 +3,7 @@ def position_taken?(board,index)
   if(board[index]==" "||board[index]==""||board[index]==nil)
     return false
   end
+  if(board[index]="X"||board[index]=="O")
+    return true
+  end
 end
